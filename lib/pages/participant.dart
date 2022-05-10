f0aa78b51e795c2b38f70920fbc733aeb368f0ad
jacksonmonteiro <infor.jackson324@gmail.com>
2022-05-10 11:22:46 -0300

    audio and video user controll

--- lib/pages/participant.dart
+++ lib/pages/participant.dart
@@ -6,6 +6,7 @@ import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 import 'package:agora_rtm/agora_rtm.dart';
 import 'package:flutter/material.dart';
 import 'package:live_stream_app/models/user.dart';
+import 'package:live_stream_app/utils/message.dart';
 import 'package:live_stream_app/utils/token.dart';
 
 import '../utils/appId.dart';
@@ -27,12 +28,13 @@ class Participant extends StatefulWidget {
 }
 
 class _ParticipantState extends State<Participant> {
-  final List<AgoraUser> _users = [];
+  List<AgoraUser> _users = [];
   late RtcEngine _engine;
   AgoraRtmClient? _client;
   AgoraRtmChannel? _channel;
   bool muted = false;
   bool videoDisabled = false;
+  bool localUserActive = false;
 
   @override
   void initState() {
@@ -120,7 +122,38 @@ class _ParticipantState extends State<Participant> {
             setState(() {
               muted = true;
             });
+            _engine.muteLocalAudioStream(true);
           }
+          break;
+        case "unmute":
+          if (parsedMessage[1] == widget.uid.toString()) {
+            setState(() {
+              muted = true;
+            });
+            _engine.muteLocalAudioStream(true);
+          }
+          break;
+        case "disabled":
+          if (parsedMessage[1] == widget.uid.toString()) {
+            setState(() {
+              videoDisabled = true;
+            });
+            _engine.muteLocalVideoStream(true);
+          }
+          break;
+        case "enable":
+          if (parsedMessage[1] == widget.uid.toString()) {
+            setState(() {
+              videoDisabled = false;
+            });
+            _engine.muteLocalVideoStream(false);
+          }
+          break;
+        case "activeUsers":
+          setState(() {
+            _users = Message().parseActiveUsers(uids: parsedMessage[1]);
+          });
+          break;
       }
 
       print('Mensagem pública de ' + member.userId + ': ' + (message.text));
@@ -148,16 +181,19 @@ class _ParticipantState extends State<Participant> {
       padding: EdgeInsets.symmetric(vertical: 48),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          RawMaterialButton(
-            onPressed: _onToggleMute,
-            child: Icon(muted ? Icons.mic_off : Icons.mic,
-                color: muted ? Colors.white : Colors.blueAccent, size: 20),
-            shape: CircleBorder(),
-            elevation: 2,
-            fillColor: muted ? Colors.blueAccent : Colors.white,
-            padding: EdgeInsets.all(12),
-          ),
+        children: <Widget>[
+          localUserActive
+              ? RawMaterialButton(
+                  onPressed: _onToggleMute,
+                  child: Icon(muted ? Icons.mic_off : Icons.mic,
+                      color: muted ? Colors.white : Colors.blueAccent,
+                      size: 20),
+                  shape: CircleBorder(),
+                  elevation: 2,
+                  fillColor: muted ? Colors.blueAccent : Colors.white,
+                  padding: EdgeInsets.all(12),
+                )
+              : SizedBox(),
           RawMaterialButton(
             onPressed: () => _onCallEnd(context),
             child: Icon(Icons.call_end, color: Colors.white, size: 20),
@@ -166,34 +202,134 @@ class _ParticipantState extends State<Participant> {
             fillColor: Colors.redAccent,
             padding: EdgeInsets.all(15),
           ),
-          RawMaterialButton(
-            onPressed: _onToggleVideoDisabled,
-            child: Icon(videoDisabled ? Icons.videocam_off : Icons.videocam,
-                color: videoDisabled ? Colors.white : Colors.blueAccent,
-                size: 20),
-            shape: CircleBorder(),
-            elevation: 2,
-            fillColor: videoDisabled ? Colors.blueAccent : Colors.white,
-            padding: EdgeInsets.all(12),
-          ),
-          RawMaterialButton(
-            onPressed: _onSwitchCamera,
-            child:
-                Icon(Icons.switch_camera, color: Colors.blueAccent, size: 20),
-            shape: CircleBorder(),
-            elevation: 2,
-            fillColor: Colors.white,
-            padding: EdgeInsets.all(12),
-          )
+          localUserActive
+              ? RawMaterialButton(
+                  onPressed: _onToggleVideoDisabled,
+                  child: Icon(
+                      videoDisabled ? Icons.videocam_off : Icons.videocam,
+                      color: videoDisabled ? Colors.white : Colors.blueAccent,
+                      size: 20),
+                  shape: CircleBorder(),
+                  elevation: 2,
+                  fillColor: videoDisabled ? Colors.blueAccent : Colors.white,
+                  padding: EdgeInsets.all(12),
+                )
+              : SizedBox(),
+          localUserActive
+              ? RawMaterialButton(
+                  onPressed: _onSwitchCamera,
+                  child: Icon(Icons.switch_camera,
+                      color: Colors.blueAccent, size: 20),
+                  shape: CircleBorder(),
+                  elevation: 2,
+                  fillColor: Colors.white,
+                  padding: EdgeInsets.all(12),
+                )
+              : SizedBox(),
         ],
       ),
     );
   }
 
+  List<Widget> _getRenderViews() {
+    final List<Widget> list = [];
+    bool checkIfLocalActive = false;
+    for (int i = 0; i < _users.length; i++) {
+      if (_users[i].uid == widget.uid) {
+        list.add(Stack(
+          children: [
+            RtcLocalView.SurfaceView(),
+            Align(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+                  color: Colors.white,
+                ),
+                child: Text(widget.userName),
+              ),
+              alignment: Alignment.bottomRight,
+            ),
+          ],
+        ));
+        checkIfLocalActive = true;
+      } else {
+        list.add(Stack(
+          children: [
+            RtcRemoteView.SurfaceView(
+              channelId: 'gfx',
+              uid: _users[i].uid,
+            ),
+            Align(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+                  color: Colors.white,
+                ),
+                child: Text(_users[i].name ?? "Erro ao mostrar nome"),
+              ),
+              alignment: Alignment.bottomRight,
+            )
+          ],
+        ));
+      }
+    }
+
+    if (checkIfLocalActive) {
+      localUserActive = true;
+    } else {
+      localUserActive = false;
+    }
+
+    return list;
+  }
+
+  Widget _expandedVideoView(List<Widget> views) {
+    final wrappedViews = views
+        .map<Widget>((view) => Expanded(
+                child: Container(
+              child: view,
+            )))
+        .toList();
+
+    return Expanded(
+        child: Row(
+      children: wrappedViews,
+    ));
+  }
+
   Widget _broadcastView() {
-    return Row(children: [
-      Expanded(child: RtcLocalView.SurfaceView()),
-    ]);
+    final views = _getRenderViews();
+
+    switch (views.length) {
+      case 1:
+        return Column(children: <Widget>[
+          _expandedVideoView([views[0]]),
+        ]);
+      case 2:
+        return Column(children: <Widget>[
+          _expandedVideoView([views[0]]),
+          _expandedVideoView([views[1]]),
+        ]);
+      case 3:
+        return Column(
+          children: <Widget>[
+            _expandedVideoView(views.sublist(0, 2)),
+            _expandedVideoView(views.sublist(2, 3))
+          ],
+        );
+      case 4:
+        return Column(
+          children: <Widget>[
+            _expandedVideoView(views.sublist(0, 2)),
+            _expandedVideoView(views.sublist(2, 4))
+          ],
+        );
+      default:
+    }
+
+    return Container();
   }
 
   void _onToggleMute() {
